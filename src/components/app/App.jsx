0465d3d17c30f0ee5c9c653a93ddb06734e0d333
asadbek755 <xushnaevasadbek@gmail.com>
2024-04-21 14:23:03 +0500

    components commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from '../button/Button'
+
+function App() {
+    return (
+      <div>
+    <h1>Hello world</h1>
+    <Button/>
+
+      </div>
+  )
+}
+
+export default App
